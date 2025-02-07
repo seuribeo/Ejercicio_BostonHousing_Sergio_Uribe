2134596bcc0baee72fdedade8a6fd3e163526cd8
seuribeo <seuribeo@unal.edu.co>
2025-02-06 23:43:14 -0500

    Update aplicacion_BostonHousing.py

--- aplicacion_BostonHousing.py
+++ aplicacion_BostonHousing.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import pickle
 import numpy as np
+import gzip
 
 # Función para cargar el modelo preentrenado
 def load_model():
     """Carga el modelo preentrenado con el mejor ajuste encontrado."""
-    with open('model_trained_regressor.pkl', 'rb') as f:
+    with gzip.open('model_trained_regressor.pkl.gz', 'rb') as f:
         model = pickle.load(f)  # Puede ser un Pipeline con StandardScaler + Kernel Ridge
     return model
     
